feb8552860a56ec92f09a118defd6ae0a1b3324d
EmmaUmeh <Emmachinemerem04@gmail.com>
2023-03-22 02:53:24 -0700

    commited Navbar files

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-// import './App.css'
 
+// import {Navlist} from "Navbar/Navbar"
+// import Navlist from './Navbar/Navbar';
+import LogoUrl  from "./images/Kutoo.svg";
+import UserImage from "./images/userImage.svg"
 import Navbar from "./Navbar/Navbar"
+
 
 function App() {
 
 
   return (
     <div className="">
-      <Navbar />
+      
+      <Navbar userName={"Emmanuel Umeh"} userTitle={"Software Engineer"} userImage={UserImage} LogoImage={LogoUrl} LogoAlt={"Kutoo image"} UserAlt={"User image"}/>
+
     </div>
   )
 }
--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -1,48 +1,133 @@
-// import React from "react";
-import LogoImage from "../images/Kutoo.svg";
-import {IoIosNotificationsOutline} from 'react-icons/io';
-import {MdOutlineLightMode, MdOutlineModeNight} from 'react-icons/md'
-import UserImage from "../images/userImage.svg"
-import '../Navbar/Navbar.css'
-const Navbar = () => {
+import styled from 'styled-components';
+import {FiSend,FiAlignJustify, FiXCircle} from 'react-icons/fi';
+import {MdOutlineDarkMode,MdOutlineLightMode} from 'react-icons/md';
+import { useState } from 'react';
+// import {BiMenuAltRight} from 'react-icons/bi';
 
-return(
+const Container = styled.div`
+width: 100%;
+background: #FFFFFF;
+padding: 8px 8px 8px 8px;
+box-shadow: 0px 4px 30px 3px rgba(0, 0, 0, 0.25);
+
+@media (max-width:480px) {
+.menubutton{
+    display: block;
+}
+.logo{
+    width:30%;
+}
+}
+`;
+
+const Flex = styled.div`
+display: flex;
+justify-content: space-between;
+align-items: center;
+`;
+
+const LogoImage = styled.div`
+width:10%;
+`;
+
+const UserInfo = styled.div `
+display:flex;
+align-items: center;
+`;
+
+const UserInfoImg = styled.div`
+width:17%;
+object-fit: cover;
+`;
+
+const UserInfoDetails = styled.div`
+display: flex;
+flex-direction: column;
+margin-right: 20px;
+`;
+
+const Button = styled.div`
+cursor:pointer;
+`;
+
+interface Userdetails{
+userName: string;
+userTitle: string;
+userImage: string;
+LogoImage: string;
+LogoAlt: string;
+UserAlt: string;
+}
+
+function Navbar(user: Userdetails) {
+    const [toggle, setToggle] = useState(false);
+
+    const [menu, setMenu] = useState(false);
+    const Menutoggle = () => setMenu(!menu)
+
+    const Handletoggle = () => setToggle(!toggle);
+    // const Handletoggle = () => (!toggle) ? `${Darkmode}` : `${LightMode}`;
+
+  return(
+    
     <>
-        <div className="Navbarcontainer">
-            <div className="Navbarchild">
-                    <div className="logoimage">
-                        <img src={LogoImage} alt="logoimage"/>
-                    </div>
+        <Container>
+            <Flex>
+                <LogoImage className='logo'>
+                    <img src={user.LogoImage} alt={user.LogoAlt}/>
+                </LogoImage>
 
-                    <div className="Userdetails">
-                        <div>
-                            <IoIosNotificationsOutline size="30"/>
-                        </div>
+                <UserInfo>
 
-                        <div>
-                            
-                            <div className="Userimage">
-                                <img src={UserImage} alt="Userimage"/>
-                            </div>
+                    <FiSend size="28" className="mr-5"/>                    
 
-                            <div>
-                                <span className="font-bold text-blue-600 username">Emmanuel Umeh</span>
-                                <span className="userprofession">Software Engineer</span>
-                            </div>
+                    <UserInfoImg className='mr-2'>
+                        <img src={user.userImage} alt={user.UserAlt}/>
+                    </UserInfoImg>
 
-                        </div>
+                       
+                          <UserInfoDetails>
+                            <div className="font-bold">
+                                    {user.userName}
+                                </div>
+                                
+                                <span>
+                                    {user.userTitle}
+                                </span>
+                          </UserInfoDetails>
+                        
+                    <Button  onClick={Handletoggle}>
+                       {
+                        toggle
+                        ?
+                        <MdOutlineDarkMode size="28"/>
+                        :
+                        <MdOutlineLightMode size="28" />
+                       }
 
-                        <div>
-                            <MdOutlineLightMode size="30"/>
-                            <MdOutlineModeNight size="30"/>
-                        </div>
-                    </div>
-            </div>
-        </div>
+                    </Button>
+                    
+                     <Button className='hidden menubutton' onClick={Menutoggle}>
+                        {
+                            menu
+                            ?
+                            < FiXCircle size="28"/>
+                            :
+                            <FiAlignJustify size="28"/>
+                        }
+                     </Button>
+
+                </UserInfo>         
+                   
+                       
+
+            </Flex>
+        </Container>
     </>
-)
-
+  )
 }
 
 export default Navbar;
+
+
 
